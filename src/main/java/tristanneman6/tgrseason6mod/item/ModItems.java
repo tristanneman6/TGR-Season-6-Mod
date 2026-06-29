caf2cc5abcbbe36408fb0705d11aae8585398e69
tristanneman6 <tristan.godart6@gmail.com>
2026-06-29 15:20:12 +0200

    Added Mechanical Wing.

--- src/main/java/tristanneman6/tgrseason6mod/item/ModItems.java
+++ src/main/java/tristanneman6/tgrseason6mod/item/ModItems.java
@@ -9,8 +9,8 @@ import net.minecraft.util.Identifier;
 import tristanneman6.tgrseason6mod.TGRSeason6BossMod;
 
 public class ModItems {
-    public static final  Item BOTTLE_OF_VOID = registerItem("bottle_of_void", new Item(new Item.Settings()));
-
+    public static final Item BOTTLE_OF_VOID = registerItem("bottle_of_void", new Item(new Item.Settings()));
+    public static final Item MECHANICAL_WING = registerItem("mechanical_wing", new Item(new Item.Settings()));
 
     private static Item registerItem(String name, Item item) {
         return Registry.register(Registries.ITEM, Identifier.of(TGRSeason6BossMod.MOD_ID, name), item);
@@ -21,6 +21,7 @@ public class ModItems {
 
         ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS).register(entries -> {
             entries.add(BOTTLE_OF_VOID);
+            entries.add(MECHANICAL_WING);
         });
     }
 }
